e82f9404dfe59da6a379a30c4c07315cbf3ff502
Амадей Воронцов <vorontsov.aa@phystech.edu>
2023-03-05 17:46:48 +0300

    delete cxx flags, which don't work in windows

--- bsort/include/sourcePath.h
+++ bsort/include/sourcePath.h
@@ -1,2 +1,2 @@
 #pragma once
-#define CL_PROGRAM_PATH "/home/amadey/Документы/Projects/OpenCL/bsort/src/bsort.cl"
+#define CL_PROGRAM_PATH "C:/Users/voron/Documents/Code/C/OpenCL/bsort/src/bsort.cl"
